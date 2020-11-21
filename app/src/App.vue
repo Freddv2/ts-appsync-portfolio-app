f41a18a6e9c333e759e35ac519d4a70ecd7acb17
fvincent <fredddv@hotmail.com>
2020-11-20 19:10:26 -0500

    WIP portfolio frontend

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,23 +1,80 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-  </div>
+  <v-app>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+        <tr>
+          <th class="text-left">
+            Name
+          </th>
+          <th class="text-left">
+            Calories
+          </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr
+            v-for="item in desserts"
+            :key="item.name"
+        >
+          <td>{{ item.name }}</td>
+          <td>{{ item.calories }}</td>
+        </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data() {
+    return {
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+        },
+      ],
+    }
+  },
+};
+</script>
---
+++ app/src/components/Portfolio.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+      <tr>
+        <th class="text-left">
+          Stock
+        </th>
+        <th class="text-left">
+          Shares
+        </th>
+        <th class="text-left">
+          Buy Price
+        </th>
+        <th class="text-left">
+          Market Price
+        </th>
+        <th class="text-left">
+          Cost
+        </th>
+        <th class="text-left">
+          Total Value
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr
+          v-for="item in stocks"
+          :key="item.stock"
+      >
+        <td>{{ item.stock }}</td>
+        <td>{{ item.shares }}</td>
+        <td>{{ item.buyPrice }}</td>
+        <td>{{ item.marketPrice }}</td>
+        <td>{{ item.buyCost }}</td>
+        <td>{{ item.totalValue }}</td>
+      </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      stocks: [],
+    }
+  },
+};
+</script>
