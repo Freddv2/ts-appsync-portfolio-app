0aee76cc97b381d6eb02d8111508ab570af35bfa
fvincent <fredddv@hotmail.com>
2020-11-21 16:39:30 -0500

    Successfully configured appsync with amplify

---
+++ app/src/graphql/queries.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getStocks = /* GraphQL */ `
+  query GetStocks {
+    getStocks {
+      portfolioId
+      stock
+      shares
+      buyPrice
+      buyCost
+      marketPrice
+      totalValue
+    }
+  }
+`;
